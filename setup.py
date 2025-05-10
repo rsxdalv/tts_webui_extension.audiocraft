dffdc6a22b8030753ed909301c040998bec56c9f
Roberts Slisans <rsxdalv@gmail.com>
2025-05-10 17:54:13 +0300

    adjust the spacy and numpy versions for cross-compatibility

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
     name="extension_audiocraft",
     packages=setuptools.find_namespace_packages(),
-    version="0.0.1",
+    version="0.0.2",
     author="rsxdalv",
     description="Audiocraft provides MusicGen and MAGNeT models for high-quality music and audio generation",
     url="https://github.com/rsxdalv/extension_audiocraft",
@@ -11,7 +11,8 @@ setuptools.setup(
     scripts=[],
     install_requires=[
         "audiocraft @ https://github.com/rsxdalv/audiocraft/releases/download/v1.4.0a1/audiocraft-1.4.0a1-py3-none-any.whl",
-        "spacy==3.7.6",
+        "spacy>=3.7.6",
+        "numpy<2", # higher numpy likely works but breaks other packages
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
